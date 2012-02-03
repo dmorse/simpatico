8b9d72ca92498d6a5a5db15071e68bace5edc589
David C. Morse <morse@node1082.localdomain>
2012-02-02 21:42:45 -0600

    Added main program to ddMd directory, and ddMd/examples.

---
+++ src/ddMd/ddSim.cpp
@@ -0,0 +1,66 @@
+#ifndef DDSIM_CPP
+#define DDSIM_CPP
+
+#include <ddMd/system/System.h>
+#include <ddMd/communicate/Domain.h>
+#include <ddMd/storage/AtomStorage.h>
+#include <ddMd/interaction/Interaction.h>
+#include <util/random/Random.h>
+#include <util/format/Dbl.h>
+#include <util/util/initStatic.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace Util;
+using namespace DdMd;
+
+int main()
+{
+
+   MPI::Init();
+   Util::initStatic();
+   Util::IntVector::commitMpiType();
+   Util::Vector::commitMpiType();
+
+   System system;
+   system.readParam(std::cin); 
+
+   Domain& domain  = system.domain();
+   AtomStorage& storage = system.atomStorage();
+   int myRank = domain.gridRank();
+
+   std::string filename("config");
+   system.readConfig(filename);
+   system.exchanger().exchangeGhosts();
+
+   system.interaction().findNeighbors();
+
+   double temperature = 1.0;
+   system.setBoltzmannVelocities(temperature);
+
+   // Calculate energies before integration
+   double kinetic   = system.kineticEnergy();
+   double potential = system.pairPotentialEnergy();
+   if (myRank == 0) {
+      std::cout << Dbl(kinetic) << Dbl(potential) 
+                << Dbl(kinetic + potential) << std::endl;
+   }
+
+   for (int i = 0; i < 5; ++i ) {
+
+      system.integrate(1000);
+
+      // Calculate energies after integration
+      kinetic   = system.kineticEnergy();
+      potential = system.pairPotentialEnergy();
+      if (myRank == 0) {
+         std::cout << Dbl(kinetic) << Dbl(potential) 
+                   << Dbl(kinetic + potential) << std::endl;
+      }
+      system.isValid();
+   }
+
+   MPI::Finalize();
+}
+#endif
