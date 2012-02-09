c5011362eb89301f7259d54cc89494a82a5c8e9b
Pavani S. Medapuram Lakshmi Narasimha <medapura@node1084.localdomain>
2012-02-09 15:39:39 -0600

    HybridNphMdMove class for Hybrid MC moves that use the NphIntegrator for MD steps
    	new file:   mcMd/mcMoves/common/HybridNphMdMove.cpp
    	new file:   mcMd/mcMoves/common/HybridNphMdMove.h

---
+++ src/mcMd/mcMoves/common/HybridNphMdMove.cpp
@@ -0,0 +1,167 @@
+#ifndef HYBRID_NPH_MD_MOVE_CPP
+#define HYBRID_NPH_MD_MOVE_CPP
+
+/*
+* Simpatico - Simulation Package for Polymeric and Molecular Liquids
+*
+* Copyright 2010, David Morse (morse@cems.umn.edu)
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "HybridNphMdMove.h"
+#include <mcMd/mcSimulation/McSystem.h>
+#include <mcMd/mdIntegrators/MdIntegrator.h>
+#include <mcMd/mdSimulation/MdSystem.h>
+#include <mcMd/simulation/Simulation.h>
+#include <mcMd/chemistry/Atom.h>
+#ifndef MCMD_NOPAIR
+#include <mcMd/potentials/pair/MdPairPotential.h>
+#include <mcMd/potentials/pair/McPairPotential.h>
+#endif
+
+namespace McMd
+{
+
+   using namespace Util;
+
+   /*
+   * Constructor
+   */
+   HybridNphMdMove::HybridNphMdMove(McSystem& system) :
+      SystemMove(system),
+      mdSystemPtr_(0),
+      nStep_(0),
+      nphIntegratorPtr_(0)
+   {
+      mdSystemPtr_ = new MdSystem(system);
+      oldPositions_.allocate(simulation().atomCapacity());
+   }
+
+   /*
+   * Destructor.
+   */
+   HybridNphMdMove::~HybridNphMdMove()
+   {
+      if (mdSystemPtr_) {
+         delete mdSystemPtr_;
+      }
+   }
+
+   /*
+   * Read parameter maxDisp
+   */
+   void HybridNphMdMove::readParam(std::istream& in)
+   {
+      readProbability(in);
+      read<int>(in, "nStep", nStep_);
+      readParamComposite(in, *mdSystemPtr_);
+      nphIntegratorPtr_ = dynamic_cast<NphIntegrator*>(&mdSystemPtr_->mdIntegrator());
+   }
+
+   /*
+   * Generate, attempt and accept or reject a Hybrid MD/MC move.
+   */
+   bool HybridNphMdMove::move()
+   {
+      System::MoleculeIterator molIter;
+      Molecule::AtomIterator   atomIter;
+      double oldEnergy, newEnergy;
+      int    iSpec;
+      int    nSpec = simulation().nSpecies();
+
+      bool   accept;
+
+      // Increment counter for attempted moves
+      incrementNAttempt();
+
+      // Store old atom positions in oldPositions_ array.
+      for (iSpec = 0; iSpec < nSpec; ++iSpec) {
+         mdSystemPtr_->begin(iSpec, molIter);
+         for ( ; molIter.notEnd(); ++molIter) {
+            for (molIter->begin(atomIter); atomIter.notEnd(); ++atomIter) {
+               oldPositions_[atomIter->id()] = atomIter->position();
+            }
+         }
+      }
+
+      // Initialize MdSystem
+      #ifndef MCMD_NOPAIR
+      mdSystemPtr_->pairPotential().buildPairList();
+      #endif
+      mdSystemPtr_->calculateForces();
+      mdSystemPtr_->setBoltzmannVelocities(energyEnsemble().temperature());
+      nphIntegratorPtr_->setup();
+      
+      // generate integrator variables from a Gaussian distribution
+      Random& random = simulation().random();
+      double temp = system().energyEnsemble().temperature();
+      double barostatMass = nphIntegratorPtr_->barostatMass();
+      if (nphIntegratorPtr_->mode() == Cubic) {
+         // one degree of freedom
+         // barostat_energy = 1/2 (1/W) eta_x^2
+         double sigma = sqrt(temp/barostatMass);
+         nphIntegratorPtr_->setEta(0, sigma*random.gaussian());
+      } else if (nphIntegratorPtr_->mode() == Tetragonal) {
+         // two degrees of freedom
+         // barostat_energy = 1/2 (1/W) eta_x^2 + 1/2 (1/(2W)) eta_y^2
+         double sigma1 = sqrt(temp/barostatMass);
+         nphIntegratorPtr_->setEta(0, sigma1*random.gaussian());
+         double sigma2 = sqrt(temp/barostatMass/2.0);
+         nphIntegratorPtr_->setEta(1, sigma2*random.gaussian());
+      } else if (nphIntegratorPtr_->mode() == Orthorhombic) { 
+         // three degrees of freedom 
+         // barostat_energy = 1/2 (1/W) (eta_x^2 + eta_y^2 + eta_z^2)
+         double sigma = sqrt(temp/barostatMass);
+         nphIntegratorPtr_->setEta(0, sigma*random.gaussian());
+         nphIntegratorPtr_->setEta(1, sigma*random.gaussian());
+         nphIntegratorPtr_->setEta(2, sigma*random.gaussian());
+      }
+
+      // Store old energy
+      oldEnergy  = mdSystemPtr_->potentialEnergy();
+      oldEnergy += mdSystemPtr_->kineticEnergy();
+
+      // Run a short MD simulation
+      for (int iStep = 0; iStep < nStep_; ++iStep) {
+         nphIntegratorPtr_->step();
+      }
+
+      // Calculate new energy
+      newEnergy  = mdSystemPtr_->potentialEnergy();
+      newEnergy += mdSystemPtr_->kineticEnergy();
+
+      // Decide whether to accept or reject
+      accept = random.metropolis( boltzmann(newEnergy-oldEnergy) );
+
+      // Accept move
+      if (accept) {
+
+         #ifndef MCMD_NOPAIR
+         // Rebuild the McSystem cellList using the new positions.
+         system().pairPotential().buildCellList();
+         #endif
+
+         // Increment counter for the number of accepted moves.
+         incrementNAccept();
+
+      } else {
+
+         // Restore old atom positions
+         for (iSpec = 0; iSpec < nSpec; ++iSpec) {
+            mdSystemPtr_->begin(iSpec, molIter);
+            for ( ; molIter.notEnd(); ++molIter) {
+               molIter->begin(atomIter);
+               for ( ; atomIter.notEnd(); ++atomIter) {
+                  atomIter->position() = oldPositions_[atomIter->id()];
+               }
+            }
+         }
+
+      }
+
+      return accept;
+
+   }
+
+}
+#endif
