82c67f4e4d136a39c12b12c6350f733e3c681ee2
David C. Morse <morse@node1083.localdomain>
2012-03-14 23:01:13 -0500

    Changed makefile system, moved ddSim to src/main.

--- src/main/sources.mk
+++ src/main/sources.mk
@@ -1,5 +1,11 @@
+
+MDSIM=$(SRC_DIR)/main/mdSim
+MCSIM=$(SRC_DIR)/main/mcSim
+DDSIM=$(SRC_DIR)/main/ddSim
+
 main_SRCS=$(SRC_DIR)/main/mdSim.cpp \
-          $(SRC_DIR)/main/mcSim.cpp
+          $(SRC_DIR)/main/mcSim.cpp \
+          $(SRC_DIR)/main/ddSim.cpp 
 
 main_OBJS=$(main_SRCS:.cpp=.o)
 
