d7c133be6e24155cfdcac9edc63636d97895c4bb
David Morse <morse012@umn.edu>
2012-11-25 18:53:28 -0600

    Adding save load to Cfb Mc Moves.

--- src/mcMd/mcMoves/linear/sources.mk
+++ src/mcMd/mcMoves/linear/sources.mk
@@ -1,7 +1,6 @@
 mcMd_mcMoves_linear_SRCS=\
     $(SRC_DIR)/mcMd/mcMoves/linear/CfbDoubleRebridgeMove.cpp \
     $(SRC_DIR)/mcMd/mcMoves/linear/CfbEndMove.cpp \
-    $(SRC_DIR)/mcMd/mcMoves/linear/CfbHomoReptationMove.cpp \
     $(SRC_DIR)/mcMd/mcMoves/linear/CfbRebridgeMove.cpp \
     $(SRC_DIR)/mcMd/mcMoves/linear/CfbReptationMove.cpp \
     $(SRC_DIR)/mcMd/mcMoves/linear/EndSwapMove.cpp 
