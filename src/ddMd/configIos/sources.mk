76986247cfde350ccb4e26bfb7d0f86e1e5f902f
David Morse <morse012@umn.edu>
2013-03-07 21:46:53 -0600

    Fixed and renamed DdMd::LammpsConfigIo. Now interchanges with Lammps in both directions.

--- src/ddMd/configIos/sources.mk
+++ src/ddMd/configIos/sources.mk
@@ -3,7 +3,7 @@ ddMd_configIos_SRCS=\
    $(SRC_DIR)/ddMd/configIos/ConfigIo.cpp \
    $(SRC_DIR)/ddMd/configIos/DdMdConfigIo.cpp \
    $(SRC_DIR)/ddMd/configIos/DdMdOrderedConfigIo.cpp \
-   $(SRC_DIR)/ddMd/configIos/LammpsOrderedConfigIo.cpp \
+   $(SRC_DIR)/ddMd/configIos/LammpsConfigIo.cpp \
    $(SRC_DIR)/ddMd/configIos/ConfigIoFactory.cpp
 
 ddMd_configIos_OBJS=$(ddMd_configIos_SRCS:.cpp=.o)
