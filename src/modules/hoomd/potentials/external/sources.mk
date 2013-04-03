dccd620e4de8d08fc6e362c1e67cdf9d58f6d35d
Pavani <medap001@umn.edu>
2013-04-02 22:06:48 -0400

    Added LocalLamellarOrderingExternal to hoomd module

--- src/modules/hoomd/potentials/external/sources.mk
+++ src/modules/hoomd/potentials/external/sources.mk
@@ -1,5 +1,6 @@
 modules_hoomd_potentials_external_SRCS=$(SRC_DIR)/modules/hoomd/potentials/external/HoomdExternalFactory.cpp \
-    $(SRC_DIR)/modules/hoomd/potentials/external/HoomdLamellarOrderingExternal.cpp 
+    $(SRC_DIR)/modules/hoomd/potentials/external/HoomdLamellarOrderingExternal.cpp \
+    $(SRC_DIR)/modules/hoomd/potentials/external/HoomdLocalLamellarOrderingExternal.cpp 
 
 
 modules_hoomd_potentials_external_OBJS=$(modules_hoomd_potentials_external_SRCS:.cpp=.o)
