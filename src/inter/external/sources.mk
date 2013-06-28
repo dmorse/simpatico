bfad4f157b906d2495b0c9439036d8808b522ac6
Pavani S. Medapuram Lakshmi Narasimha <medap001@umn.edu>
2013-06-28 17:16:42 -0500

    Replaced PeriodicExternal with OrderingExternal

--- src/inter/external/sources.mk
+++ src/inter/external/sources.mk
@@ -1,10 +1,9 @@
 inter_external_SRCS=\
     $(SRC_DIR)/inter/external/BoxExternal.cpp \
-    $(SRC_DIR)/inter/external/OrthoBoxExternal.cpp \
     $(SRC_DIR)/inter/external/SlitExternal.cpp \
     $(SRC_DIR)/inter/external/LamellarOrderingExternal.cpp \
     $(SRC_DIR)/inter/external/LocalLamellarOrderingExternal.cpp \
-    $(SRC_DIR)/inter/external/PeriodicExternal.cpp 
+    $(SRC_DIR)/inter/external/OrderingExternal.cpp 
 
 inter_external_OBJS=$(inter_external_SRCS:.cpp=.o)
 
