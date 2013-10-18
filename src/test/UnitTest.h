69f035dadb13f34872631befeb6c76e2faff1284
David Morse <morse012@umn.edu>
2013-10-18 08:42:10 -0500

    Removed test framework function definitions from class body.

--- src/test/UnitTest.h
+++ src/test/UnitTest.h
@@ -52,124 +52,87 @@ public:
    /**
    * Constructor
    */
-   UnitTest() :
-      #ifdef TEST_MPI      
-      communicatorPtr_(0),
-      mpiRank_(-1),
-      #endif
-      verbose_(0),
-      isIoProcessor_(true)
-   {
-      #ifdef TEST_MPI
-      // Set the communicator to COMM_WORLD by default.
-      setCommunicator(MPI::COMM_WORLD);
-      #endif
-   }
+   UnitTest();
 
    /**
    * Destructor.
    */
-   virtual ~UnitTest() 
-   {}
+   virtual ~UnitTest();
 
    /**
    * Set up before each test method (empty default implementation).
    */
-   virtual void setUp()
-   {}
+   virtual void setUp();
 
    /**
    * Tear down after each test method (empty default implementation).
    */
-   virtual void tearDown()
-   {}
+   virtual void tearDown();
 
    /**
    * Set verbosity level.
    *
    * \param verbose verbosity level (0 = silent).
    */
-   void setVerbose(int verbose)
-   {  verbose_ = verbose; }
+   void setVerbose(int verbose);
 
    /**
    * Set file prefix.
    */
-   void setFilePrefix(const std::string& prefix)
-   {  filePrefix_  = prefix; }
+   void setFilePrefix(const std::string& prefix);
 
    /**
    * Get file prefix string
    */
-   const std::string& filePrefix()
-   {  return filePrefix_; }
+   const std::string& filePrefix();
 
    /**
    * Should this processor read and write to file?
    */
-   bool isIoProcessor() const
-   {  return isIoProcessor_; } 
+   bool isIoProcessor() const;
 
    #ifdef TEST_MPI
-   void setCommunicator(MPI::Intracomm& communicator)
-   {  
-      communicatorPtr_ = &communicator; 
-      mpiRank_ = communicator.Get_rank();
-      if (mpiRank_ == 0) {
-         isIoProcessor_ = true; 
-      } else {
-         isIoProcessor_ = false; 
-      }
-   }
+   /**
+   * Set the MPI communicator.
+   *
+   * \param communicator MPI communicator object
+   */
+   void setCommunicator(MPI::Intracomm& communicator);
 
    /**
    * Return rank of this processor in communicator.
    */
-   int mpiRank()
-   {  return mpiRank_; } 
+   int mpiRank();
 
    /**
    * Does this test have a communicator?
    */
-   bool hasCommunicator()
-   {  return bool(communicatorPtr_ != 0); }
+   bool hasCommunicator();
 
    /**
    * Return the communicator by reference.
    */
-   MPI::Intracomm& communicator()
-   {  return *communicatorPtr_; }
+   MPI::Intracomm& communicator();
    #endif
 
 protected:
 
    /**
    * Write name of a class method, iff ioProcessor.
+   *
+   * \param methodName name of class test method
    */
-   void printMethod(const char* methodName)
-   {  if (isIoProcessor()) {
-         std::cout << std::endl;
-         std::cout << std::string(methodName); 
-      }
-   }
+   void printMethod(const char* methodName);
 
    /**
    * Write carriage return, iff isIoProcessor.
    */
-   void printEndl()
-   {  if (isIoProcessor()) std::cout << std::endl; } 
+   void printEndl();
 
    /**
    * Print a line of hashes, iff isIoProcessor.
    */
-   virtual void endMarker()
-   {
-      if (isIoProcessor()) {
-         std::cout << std::endl;
-         std::cout << "----------------------------------------------------";
-         std::cout << std::endl << std::endl;
-      }
-   }
+   virtual void endMarker();
 
    /**
    * Open input file.
@@ -180,18 +143,7 @@ protected:
    * \param name base file name (added to filePrefix).
    * \param in input file (opened on return).
    */
-   void openInputFile(const std::string& name, std::ifstream& in) const
-   {   
-      std::string filename = filePrefix_;
-      filename += name;
-      in.open(filename.c_str());
-      if (in.fail()) {
-         std::cout << std::endl;
-         std::cout << "Failure to open input file " 
-                   << filename << std::endl;
-         TEST_THROW("Failure to open file");
-      }
-   }
+   void openInputFile(const std::string& name, std::ifstream& in) const;
 
    /**
    * Open output file stream.
@@ -202,39 +154,22 @@ protected:
    * \param name base file name (added to filePrefix).
    * \param out  output file (opened on return).
    */
-   void openOutputFile(const std::string& name, std::ofstream& out) const
-   {   
-      std::string filename = filePrefix_;
-      filename += name;
-      out.open(filename.c_str());
-      if (out.fail()) {
-         std::cout << std::endl;
-         std::cout << "Failure to open output file " 
-                   << filename << std::endl;
-         TEST_THROW("Failure to open file");
-      }
-   }
+   void openOutputFile(const std::string& name, std::ofstream& out) const;
 
    /**
    * Return integer verbosity level  (0 == silent).
    */
-   int verbose() const
-   {  return verbose_; }
+   int verbose() const;
  
    /**
    * Return true if two integers are equal.
    */
-   static bool eq(int s1, int s2)
-   {  return (s1 == s2); }
+   static bool eq(int s1, int s2);
 
    /**
    * Return true if two double precision floats are equal.
    */
-   static bool eq(double s1, double s2)
-   {
-      double epsilon = 1.0E-10; 
-      return ( fabs(s1-s2) < epsilon ); 
-   }
+   static bool eq(double s1, double s2);
 
 private:
 
@@ -256,4 +191,185 @@ private:
    bool isIoProcessor_;
 
 };
+
+/*
+* Constructor
+*/
+UnitTest::UnitTest() :
+   #ifdef TEST_MPI      
+   communicatorPtr_(0),
+   mpiRank_(-1),
+   #endif
+   verbose_(0),
+   isIoProcessor_(true)
+{
+   #ifdef TEST_MPI
+   // Set the communicator to COMM_WORLD by default.
+   setCommunicator(MPI::COMM_WORLD);
+   #endif
+}
+
+/*
+* Destructor.
+*/
+UnitTest::~UnitTest() 
+{}
+
+/*
+* Set up before each test method (empty default implementation).
+*/
+void UnitTest::setUp()
+{}
+
+/*
+* Tear down after each test method (empty default implementation).
+*/
+void UnitTest::tearDown()
+{}
+
+/*
+* Set verbosity level.
+*
+* \param verbose verbosity level (0 = silent).
+*/
+void UnitTest::setVerbose(int verbose)
+{  verbose_ = verbose; }
+
+/*
+* Set file prefix.
+*/
+void UnitTest::setFilePrefix(const std::string& prefix)
+{  filePrefix_  = prefix; }
+
+/*
+* Get file prefix string
+*/
+const std::string& UnitTest::filePrefix()
+{  return filePrefix_; }
+
+/*
+* Should this processor read and write to file?
+*/
+bool UnitTest::isIoProcessor() const
+{  return isIoProcessor_; } 
+
+#ifdef TEST_MPI
+/*
+* Set the MPI communicator.
+*/
+void UnitTest::setCommunicator(MPI::Intracomm& communicator)
+{  
+   communicatorPtr_ = &communicator; 
+   mpiRank_ = communicator.Get_rank();
+   if (mpiRank_ == 0) {
+      isIoProcessor_ = true; 
+   } else {
+      isIoProcessor_ = false; 
+   }
+}
+
+/*
+* Return rank of this processor in communicator.
+*/
+int UnitTest::mpiRank()
+{  return mpiRank_; } 
+
+/*
+* Does this test have a communicator?
+*/
+bool UnitTest::hasCommunicator()
+{  return bool(communicatorPtr_ != 0); }
+
+/*
+* Return the communicator by reference.
+*/
+MPI::Intracomm& UnitTest::communicator()
+{  return *communicatorPtr_; }
+#endif
+
+/*
+* Write name of a class method, iff ioProcessor.
+*/
+void UnitTest::printMethod(const char* methodName)
+{  if (isIoProcessor()) {
+      std::cout << std::endl;
+      std::cout << std::string(methodName); 
+   }
+}
+
+/*
+* Write carriage return, iff isIoProcessor.
+*/
+void UnitTest::printEndl()
+{  if (isIoProcessor()) std::cout << std::endl; } 
+
+/*
+* Print a line of hashes, iff isIoProcessor.
+*/
+void UnitTest::endMarker()
+{
+   if (isIoProcessor()) {
+      std::cout << std::endl;
+      std::cout << "----------------------------------------------------";
+      std::cout << std::endl << std::endl;
+   }
+}
+
+/*
+* Open input file.
+*/
+void 
+UnitTest::openInputFile(const std::string& name, std::ifstream& in) 
+const
+{   
+   std::string filename = filePrefix_;
+   filename += name;
+   in.open(filename.c_str());
+   if (in.fail()) {
+      std::cout << std::endl;
+      std::cout << "Failure to open input file " 
+                << filename << std::endl;
+      TEST_THROW("Failure to open file");
+   }
+}
+
+/*
+* Open output file stream.
+*/
+void 
+UnitTest::openOutputFile(const std::string& name, std::ofstream& out) 
+const
+{   
+   std::string filename = filePrefix_;
+   filename += name;
+   out.open(filename.c_str());
+   if (out.fail()) {
+      std::cout << std::endl;
+      std::cout << "Failure to open output file " 
+                << filename << std::endl;
+      TEST_THROW("Failure to open file");
+   }
+}
+
+/*
+* Return integer verbosity level  (0 == silent).
+*/
+inline int UnitTest::verbose() const
+{  return verbose_; }
+ 
+/*
+* Return true if two integers are equal (static).
+*/
+inline bool UnitTest::eq(int s1, int s2)
+{  return (s1 == s2); }
+
+/*
+* Return true if two double precision floats are equal (static).
+*/
+bool UnitTest::eq(double s1, double s2)
+{
+   double epsilon = 1.0E-10; 
+   return ( fabs(s1-s2) < epsilon ); 
+}
+
 #endif
