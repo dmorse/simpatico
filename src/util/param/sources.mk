196c30668f94ddd2267b5ff93bde45f0590dd456
David Morse <morse012@umn.edu>
2013-10-24 20:38:15 -0500

    Editing modules/sliplink makefiles.

--- src/util/param/sources.mk
+++ src/util/param/sources.mk
@@ -6,5 +6,5 @@ util_param_=util/param/Begin.cpp \
     util/param/Parameter.cpp 
 
 util_param_SRCS=$(addprefix $(SRC_DIR)/, $(util_param_))
-util_param_OBJS=$(addprefix $(SRC_DIR)/, $(util_param_:.cpp=.o))
+util_param_OBJS=$(addprefix $(BLD_DIR)/, $(util_param_:.cpp=.o))
 
