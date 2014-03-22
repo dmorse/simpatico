a66fbea881a6606700748a5166eec24d1a1ccb83
taher ghasimakbari <taherghasimakbari@fedora17.microway.com>
2014-03-21 20:51:05 -0500

    The new external Potential is add and the old version is also save under the name SimplePeriodicPotential.

--- src/inter/external/SimplePeriodicExternal.cpp
+++ src/inter/external/SimplePeriodicExternal.cpp
@@ -1,5 +1,5 @@
-#ifndef INTER_PERIODIC_EXTERNAL_CPP
-#define INTER_PERIODIC_EXTERNAL_CPP
+#ifndef INTER_SIMPLE_PERIODIC_EXTERNAL_CPP
+#define INTER_SIMPLE_PERIODIC_EXTERNAL_CPP
 
 /*
 * Simpatico - Simulation Package for Polymeric and Molecular Liquids
@@ -8,7 +8,7 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "PeriodicExternal.h"
+#include "SimplePeriodicExternal.h"
 
 #include <iostream>
 
@@ -20,24 +20,22 @@ namespace Inter
    /* 
    * Constructor.
    */
-   PeriodicExternal::PeriodicExternal() 
+   SimplePeriodicExternal::SimplePeriodicExternal() 
     : externalParameter_(),
       nWaveVectors_(),
-      C_(),
       periodicity_(),
       interfaceWidth_(),
       boundaryPtr_(0),
       nAtomType_(0), 
       isInitialized_(false)
-   { setClassName("PeriodicExternal"); }
+   { setClassName("SimplePeriodicExternal"); }
    
    /* 
    * Copy constructor.
    */
-   PeriodicExternal::PeriodicExternal(const PeriodicExternal& other)
+   SimplePeriodicExternal::SimplePeriodicExternal(const SimplePeriodicExternal& other)
     : externalParameter_(other.externalParameter_),
       nWaveVectors_(other.nWaveVectors_),
-      C_(other.C_),
       periodicity_(other.periodicity_),
       interfaceWidth_(other.interfaceWidth_),
       boundaryPtr_(other.boundaryPtr_),
@@ -48,29 +46,21 @@ namespace Inter
       for (int i=0; i < nAtomType_; ++i) {
         prefactor_[i] = other.prefactor_[i];
       }
-      waveVectors_.allocate(nWaveVectors_);
+      waveIntVectors_.allocate(nWaveVectors_);
       for (int j=0; j < Dimension; ++j) {
         for (int i=0; i < nWaveVectors_; ++i) {
-          waveVectors_[i][j] = other.waveVectors_[i][j];
+          waveIntVectors_[i][j] = other.waveIntVectors_[i][j];
         }
-      }
-      phases_.allocate(nWaveVectors_);
-      for (int i=0; i < nWaveVectors_; ++i) {
-        phases_[i] = other.phases_[i];
-      }
-      for (int i=0; i < Dimension; ++i) {
-        shift_[i] = other.shift_[i];
       }
    } 
      
    /* 
    * Assignment operator.
    */
-   PeriodicExternal& PeriodicExternal::operator = (const PeriodicExternal& other)
+   SimplePeriodicExternal& SimplePeriodicExternal::operator = (const SimplePeriodicExternal& other)
    {
       externalParameter_   = other.externalParameter_;
       nWaveVectors_        = other.nWaveVectors_;
-      C_                   = other.C_;
       periodicity_         = other.periodicity_;
       interfaceWidth_      = other.interfaceWidth_;
       boundaryPtr_         = other.boundaryPtr_;
@@ -81,15 +71,8 @@ namespace Inter
       }
       for (int j=0; j < Dimension; ++j) {
         for (int i=0; i < nWaveVectors_; ++i) {
-          waveVectors_[i][j] = other.waveVectors_[i][j];
+          waveIntVectors_[i][j] = other.waveIntVectors_[i][j];
         }
-      }
-      phases_.allocate(nWaveVectors_);
-      for (int i=0; i < nWaveVectors_; ++i) {
-        phases_[i] = other.phases_[i];
-      }
-      for (int i=0; i < Dimension; ++i) {
-        shift_[i] = other.shift_[i];
       }
       return *this;
    }
@@ -97,22 +80,22 @@ namespace Inter
    /* 
    * Set nAtomType
    */
-   void PeriodicExternal::setNAtomType(int nAtomType) 
+   void SimplePeriodicExternal::setNAtomType(int nAtomType) 
    {  
       if (nAtomType <= 0) {
          UTIL_THROW("nAtomType <= 0");
       }
       if (nAtomType > MaxAtomType) {
-         UTIL_THROW("nAtomType > PeriodicExternal::MaxAtomType");
+         UTIL_THROW("nAtomType > SimplePeriodicExternal::MaxAtomType");
       }
       nAtomType_ = nAtomType;
    }
 
-   void PeriodicExternal::setExternalParameter(double externalParameter) 
+   void SimplePeriodicExternal::setExternalParameter(double externalParameter) 
    {  
       // Preconditions
       if (!isInitialized_) {
-         UTIL_THROW("PeriodicExternal potential is not initialized");
+         UTIL_THROW("SimplePeriodicExternal potential is not initialized");
       }
      
       externalParameter_ = externalParameter;
@@ -122,13 +105,13 @@ namespace Inter
    /* 
    * Set pointer to the Boundary.
    */
-   void PeriodicExternal::setBoundary(Boundary &boundary)
+   void SimplePeriodicExternal::setBoundary(Boundary &boundary)
    {  boundaryPtr_ = &boundary; }
    
    /* 
    * Read potential parameters from file.
    */
-   void PeriodicExternal::readParameters(std::istream &in) 
+   void SimplePeriodicExternal::readParameters(std::istream &in) 
    {
       if (nAtomType_ == 0) {
          UTIL_THROW("nAtomType must be set before readParam");
@@ -144,12 +127,9 @@ namespace Inter
       read<double>(in, "externalParameter", externalParameter_);
 
       read<int>(in, "nWaveVectors", nWaveVectors_);
-      read<double>(in, "C", C_);
-      waveVectors_.allocate(nWaveVectors_);
-      readDArray<Vector>(in, "waveVectors", waveVectors_, nWaveVectors_);
+      waveIntVectors_.allocate(nWaveVectors_);
+      readDArray<IntVector>(in, "waveIntVectors", waveIntVectors_, nWaveVectors_);
       
-      readDArray<double>(in, "phases", phases_, nWaveVectors_);
-      read<Vector>(in, "shift", shift_);
       read<double>(in, "interfaceWidth", interfaceWidth_);
       read<int>(in, "periodicity", periodicity_);
 
@@ -159,7 +139,7 @@ namespace Inter
    /*
    * Load internal state from an archive.
    */
-   void PeriodicExternal::loadParameters(Serializable::IArchive &ar)
+   void SimplePeriodicExternal::loadParameters(Serializable::IArchive &ar)
    {
       ar >> nAtomType_;
       if (nAtomType_ <= 0) {
@@ -168,11 +148,8 @@ namespace Inter
       prefactor_.allocate(nAtomType_);
       loadDArray<double>(ar, "prefactor", prefactor_, nAtomType_);
       loadParameter<double>(ar, "externalParameter", externalParameter_);
-      loadParameter<double>(ar, "C", C_);
-      waveVectors_.allocate(nWaveVectors_);
-      loadDArray<Vector>(ar, "waveVectors", waveVectors_, nWaveVectors_);
-      loadDArray<double>(ar, "phases", phases_, nWaveVectors_);
-      loadParameter<Vector>(ar, "shift", shift_);
+      waveIntVectors_.allocate(nWaveVectors_);
+      loadDArray<IntVector>(ar, "waveIntVectors", waveIntVectors_, nWaveVectors_);
       loadParameter<double>(ar, "interfaceWidth", interfaceWidth_);
       loadParameter<int>(ar, "periodicity", periodicity_);
       isInitialized_ = true;
@@ -181,30 +158,27 @@ namespace Inter
    /*
    * Save internal state to an archive.
    */
-   void PeriodicExternal::save(Serializable::OArchive &ar)
+   void SimplePeriodicExternal::save(Serializable::OArchive &ar)
    {
       ar << nAtomType_;
       ar << prefactor_;
       ar << externalParameter_;
-      ar << C_;
-      ar << waveVectors_;
-      ar << phases_;
-      ar << shift_;
+      ar << waveIntVectors_;
       ar << interfaceWidth_;
       ar << periodicity_;
    }
 
 
-   double PeriodicExternal::externalParameter() const
+   double SimplePeriodicExternal::externalParameter() const
    { 
      return externalParameter_; 
    }
 
    /*
-   * Return name string "PeriodicExternal".
+   * Return name string "SimplePeriodicExternal".
    */
-   std::string PeriodicExternal::className() const
-   {  return std::string("PeriodicExternal"); }
+   std::string SimplePeriodicExternal::className() const
+   {  return std::string("SimplePeriodicExternal"); }
  
 } 
 #endif
