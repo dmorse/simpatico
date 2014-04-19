fe65a2db7aecb349b61ea7c0b0e3d0f2d492398c
David Morse <morse012@umn.edu>
2014-04-19 11:17:42 -0500

    Edited ChainMaker to output species, molecule, atom ids in that order.

--- tools/cpp/chainMaker/ChainMaker.cpp
+++ tools/cpp/chainMaker/ChainMaker.cpp
@@ -28,6 +28,12 @@ void ChainMaker::writeChains(std::ostream& out)
    } else 
    if (outputStyle_ == "DdMd") {
       writeChainsDdMd(out);
+   } else
+   if (outputStyle_ == "DdMdMolecule") {
+      writeChainsDdMdMole(out);
+   } else {
+      std::cout << "Unrecognized style " 
+                << outputStyle_ << std::endl;
    }
 }
 
@@ -109,6 +115,110 @@ void ChainMaker::writeChainsDdMd(std::ostream& out)
 
          ++i;
       }
+   }
+
+   // Write bonds
+   out << std::endl;
+   out << "BONDS" << std::endl;
+   out << "nBond  " << nMolecule_*(nAtomPerMolecule_ -1 ) << std::endl;
+   i = 0;
+   j = 0;
+   for (iMol = 0; iMol < nMolecule_; ++iMol) {
+      for (iAtom = 0; iAtom < nAtomPerMolecule_ - 1; ++iAtom) {
+         out << Int(j,5) <<  Int(bondType, 5) << "  ";
+         out << Int(i, 10) << Int(i + 1, 10) << std::endl;
+         ++i;
+         ++j;
+      }
+      ++i;
+   }
+
+   // Write angles
+   int angleType = 0;
+   out << std::endl;
+   out << "ANGLES" << std::endl;
+   out << "nAngle  " << nMolecule_*(nAtomPerMolecule_ -2) << std::endl;
+   i = 0;
+   j = 0;
+   for (iMol = 0; iMol < nMolecule_; ++iMol) {
+      for (iAtom = 0; iAtom < nAtomPerMolecule_ - 2; ++iAtom) {
+         out << Int(j,5) <<  Int(angleType, 5) << "  ";
+         out << Int(i, 10) << Int(i + 1, 10) << Int(i + 2, 10)
+             << std::endl;
+         ++i;
+         ++j;
+      }
+      i += 2;
+   }
+
+   // Write dihedrals
+   int dihedralType = 0;
+   out << std::endl;
+   out << "DIHEDRALS" << std::endl;
+   out << "nDihedral  " << nMolecule_*(nAtomPerMolecule_ -3) << std::endl;
+   i = 0;
+   j = 0;
+   for (iMol = 0; iMol < nMolecule_; ++iMol) {
+      for (iAtom = 0; iAtom < nAtomPerMolecule_ - 3; ++iAtom) {
+         out << Int(j,5) <<  Int(dihedralType, 5) << "  ";
+         out << Int(i, 10) << Int(i + 1, 10) << Int(i + 2, 10)
+             << Int(i + 3, 10) << std::endl;
+         ++i;
+         ++j;
+      }
+      i += 3;
+   }
+
+}
+
+void ChainMaker::writeChainsDdMdMole(std::ostream& out)
+{
+   Vector r;
+   Vector velocity;
+   Vector v; 
+   double beta = 1.0;
+   int atomType = 0;
+   int sId = 0;
+   int bondType = 0;
+   int iMol, iAtom, i, j;
+
+   out << "BOUNDARY" << std::endl;
+   out << std::endl;
+   out << boundary_ << std::endl;
+   out << std::endl;
+   out << "ATOMS" << std::endl;
+   out << "nAtom  " << nMolecule_*nAtomPerMolecule_ << std::endl;
+
+   i = 0;
+   velocity.zero();
+   for (iMol = 0; iMol < nMolecule_; ++iMol) {
+
+      boundary_.randomPosition(random_, r);
+
+      for (iAtom = 0; iAtom < nAtomPerMolecule_; ++iAtom) {
+
+         out << Int(i,10) << Int(atomType, 6)
+             << Int(sId,6) << Int(iMol, 10) << Int(iAtom,6);
+         out << "\n";
+         for (j = 0; j < Dimension; ++j) {
+            out << Dbl(r[j], 15, 6);
+         }
+         out << "\n";
+         for (j = 0; j < Dimension; ++j) {
+            out << Dbl(0.0, 15, 6);
+         }
+         out << "\n";
+
+         if (iAtom < nAtomPerMolecule_ - 1) {
+            random_.unitVector(v);
+            v *= bondPotential_.randomBondLength(&random_, beta, bondType);
+            r += v;
+            boundary_.shift(r);
+         }
+
+         ++i;
+      }
+      out << std::endl;
    }
 
    // Write bonds
