90f6aabbf40992ef305e112979f17f9e52f44686
David Morse <morse012@umn.edu>
2014-04-18 23:51:24 -0500

    Adding documentation ot AtomContext struct.

--- src/ddMd/chemistry/AtomContext.h
+++ src/ddMd/chemistry/AtomContext.h
@@ -11,11 +11,39 @@
 #ifdef DDMD_MOLECULES
 
 namespace DdMd{
+
+   /**
+   * Descriptor for context of an Atom within a molecule and species.
+   *
+   * The information in the AtomContext struct is not used by any 
+   * of of the essential algorithms of ddSim, but may be used in the
+   * implementation of some analyzers or modifiers.
+   */
    struct AtomContext{
+
+      /**
+      * Index of species of molecule containing atom.
+      */
       int speciesId;
+
+      /**
+      * Index of molecule within its molecular species.
+      */
       int moleculeId;
+
+      /**
+      * Index of atom within its parent molecule.
+      *
+      * Note: This takes on values from zero to the number of
+      * of atoms within the molecule. Corresponding atoms in
+      * identical molecules have the same atomId. This index is
+      * thus distinct from the global atom id, which is unique
+      * within the system.
+      */
       int atomId;
+
    };
+
 }
 
 #endif
