7a4b557c222f3bd7db6e3af82ee3b1d0521d0f99
David Morse <morse012@umn.edu>
2014-10-27 09:59:57 -0500

    Edit makefiles to clean files generated by tests

--- src/mcMd/tests/perturb/sources.mk
+++ src/mcMd/tests/perturb/sources.mk
@@ -1,4 +1,7 @@
-tests_mcMd_perturb_SRCS=$(TESTS_DIR)/mcMd/perturb/Test.cc 
+mcMd_tests_perturb_=mcMd/tests/perturb/Test.cc 
 
-tests_mcMd_perturb_OBJS=$(tests_mcMd_perturb_SRCS:.cc=.o)
+mcMd_tests_perturb_SRCS=\
+     $(addprefix $(SRC_DIR)/, $(mcMd_tests_perturb_))
+mcMd_tests_perturb_OBJS=\
+     $(addprefix $(BLD_DIR)/, $(mcMd_tests_perturb_:.cc=.o))
 
