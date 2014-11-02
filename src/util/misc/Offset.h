13f76ea555ea00ae0e4d2dcf0325846d69fa2a50
David Morse <morse012@umn.edu>
2014-11-02 12:19:11 -0600

    Updating copyright year

--- src/util/misc/Offset.h
+++ src/util/misc/Offset.h
@@ -6,7 +6,7 @@
 /*
 * Simpatico - Simulation Package for Polymeric and Molecular Liquids
 *
-* Copyright 2010 - 2012, The Regents of the University of Minnesota
+* Copyright 2010 - 2014, The Regents of the University of Minnesota
 * Distributed under the terms of the GNU General Public License.
 */
 
