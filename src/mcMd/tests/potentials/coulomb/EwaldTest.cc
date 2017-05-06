41b3717f69b21adcc83eec701325f2fcbf4a6d2f
David Morse <morse012@umn.edu>
2017-05-06 10:54:44 -0500

    Work on EwaldTest

--- src/mcMd/tests/potentials/coulomb/EwaldTest.cc
+++ src/mcMd/tests/potentials/coulomb/EwaldTest.cc
@@ -12,44 +12,95 @@ using namespace Util;
 class EwaldTest 
 {
 
-   MdSimulation sim;
-
 public:
 
+   /**
+   * Default constructor.
+   */
+   EwaldTest()
+    : nAtom_(0)
+   {}
 
-   void setup() 
+   /**
+   * Read parameter file and initialize
+   */
+   void readParam(std::string paramFileName) 
    {
       // Read parameter file
       std::ifstream in;
-      sim.fileMaster().openInputFile("in/param", in);
+      sim.fileMaster().openInputFile(paramFileName, in);
       sim.readParam(in);
+      // double diameter;
+      // in >> diameter;
       in >> sim.system().boundary();
       in.close();
       std::cout << "Finished reading param file" << std::endl;
 
-      // Generate an initial configuration
-      DArray<int> capacities; 
-      int nSpecies = sim.nSpecies();
-      capacities.allocate(nSpecies);
-      DArray<double> diameters; 
+      // Allocate and initialize diameters_ array
       int nAtomType = sim.nAtomType();
-      diameters.allocate(nAtomType);
-      for (int i = 0; i < nSpecies; ++i) {
-         capacities[i] = sim.species(i).capacity();
-      }
+      diameters_.allocate(nAtomType);
       for (int i = 0; i < nAtomType; ++i) {
-         diameters[i] = 0.2;
+         diameters_[i] = 0.2;
       }
-      std::cout << "Begin generating molecules" << std::endl;
-      sim.system().generateMolecules(capacities, diameters);
-      std::cout << "Finished generating molecules" << std::endl;
 
-      double alphaMin = 0.5;
-      double dAlpha   = 0.05;
-      double alpha, kEnergy, rEnergy, energy;
+      // Allocate and initialize capacities_ array
+      int nSpecies = sim.nSpecies();
+      capacities_.allocate(nSpecies);
+      nAtom_ = 0;
+      for (int i = 0; i < nSpecies; ++i) {
+         capacities_[i] = sim.species(i).capacity();
+         nAtom_ += capacities_[i];
+      }
+
+      // Allocate and initialize forces_ array
+      forces_.allocate(nAtom_);
+      for (int i = 0; i < nAtom_; ++i) {
+         forces_[i].zero();
+      }
+
+      MdCoulombPotential& coulomb = sim.system().coulombPotential();
+      alpha_ = coulomb.get("alpha");
+      rSpaceCutoff_ = coulomb.get("rSpaceCutoff");
+      kSpaceCutoff_ = coulomb.get("kSpaceCutoff");
+   }
+
+   void generateConfig() 
+   {
+      std::cout << "Begin generating molecules" << std::endl;
+      sim.system().generateMolecules(capacities_, diameters_);
+      std::cout << "Finished generating molecules" << std::endl;
+   };
+
+   void storeForces(DArray<Vector> forces) 
+   {
+      MdSystem& system = sim.system();
+      System::MoleculeIterator molIter;
+      Molecule::AtomIterator atomIter;
+      int iAtom = 0;
+      for (int iSpecies = 0; iSpecies < sim.nSpecies(); ++iSpecies) {
+         for (system.begin(iSpecies, molIter); molIter.notEnd(); ++molIter) {
+            for (molIter->begin(atomIter); atomIter.notEnd(); ++molIter) {
+               forces[iAtom] = atomIter->force();
+               ++iAtom;
+            }
+         }
+      }
+   }
+
+   void varyAlpha(double alphaMin, double alphaMax, int n)
+   {
       MdCoulombPotential& coulomb = sim.system().coulombPotential();
       MdPairPotential& pair = sim.system().pairPotential();
-      for (int i = 0; i < 20; ++i) {
+
+      // Compute well converged reference energy
+      coulomb.unsetEnergy();
+      pair.unsetEnergy();
+      double energyRef = coulomb.energy();
+
+      // Loop over values of alpha
+      double dAlpha = (alphaMax - alphaMin)/double(n);
+      double alpha, kEnergy, rEnergy, energy;
+      for (int i = 0; i <= n; ++i) {
          alpha = alphaMin + dAlpha*i;
          coulomb.set("alpha", alpha);
          coulomb.unsetEnergy();
@@ -57,13 +108,58 @@ public:
          kEnergy = coulomb.kSpaceEnergy();
          rEnergy = coulomb.rSpaceEnergy();
          energy = kEnergy + rEnergy;
-         std::cout << Dbl(coulomb.get("alpha"),15)
-                   << "  " << Dbl(kEnergy, 15)
-                   << "  " << Dbl(rEnergy, 15)
-                   << "  " << energy
+         std::cout << Dbl(coulomb.get("alpha"), 10)
+                   << "  " << Dbl(kEnergy, 20, 13)
+                   << "  " << Dbl(rEnergy, 20, 13)
+                   << "  " << Dbl(energy, 20, 13) 
+                   << "  " << Dbl(energy - energyRef, 20, 13) 
                    << std::endl;
       }
+
+      // Reset to default
+      coulomb.set("alpha", alpha_);
    }
+
+   void varyRSpaceCutoff(double rSpaceCutoffMin, double rSpaceCutoffMax, int n)
+   {
+      MdCoulombPotential& coulomb = sim.system().coulombPotential();
+      MdPairPotential& pair = sim.system().pairPotential();
+
+      double dRSpaceCutoff = (rSpaceCutoffMax - rSpaceCutoffMin)/double(n);
+      double rSpaceCutoff, kEnergy, rEnergy, energy;
+      for (int i = 0; i <= n; ++i) {
+         rSpaceCutoff = rSpaceCutoffMin + dRSpaceCutoff*i;
+         coulomb.set("rSpaceCutoff", rSpaceCutoff);
+         coulomb.unsetEnergy();
+         pair.unsetEnergy();
+         kEnergy = coulomb.kSpaceEnergy();
+         rEnergy = coulomb.rSpaceEnergy();
+         energy = kEnergy + rEnergy;
+         std::cout << Dbl(coulomb.get("rSpaceCutoff"), 10)
+                   << "  " << Dbl(kEnergy, 20)
+                   << "  " << Dbl(rEnergy, 20)
+                   << "  " << Dbl(energy, 20)
+                   << std::endl;
+      }
+
+      // Reset to default
+      coulomb.set("rSpaceCutoff", rSpaceCutoff_);
+   }
+
+   MdSimulation& simulation()
+   {  return sim; }
+
+private:
+
+   MdSimulation sim;
+
+   DArray<int> capacities_; 
+   DArray<double> diameters_; 
+   DArray<Vector> forces_;
+   double alpha_;
+   double rSpaceCutoff_;
+   double kSpaceCutoff_;
+   int nAtom_;
 
 };
 
@@ -72,7 +168,11 @@ int main(int argc, char* argv[])
 
    EwaldTest test;
 
-   test.setup();
-   
+   test.readParam("in/param");
+   test.generateConfig();
+   test.varyAlpha(0.2, 3.0, 28);
+   //std::cout << "alpha = " 
+   //          << test.simulation().system().coulombPotential().get("alpha")
+   //          << std::endl;
 
 }
