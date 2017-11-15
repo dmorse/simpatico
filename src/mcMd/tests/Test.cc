a0cf28fe39a8ae9dc67f5453cbe840f01d572e4c
David Morse <morse012@umn.edu>
2017-11-15 14:00:39 -0600

    Adding explicit return values to simp, mcMd tests

--- src/mcMd/tests/Test.cc
+++ src/mcMd/tests/Test.cc
@@ -21,14 +21,25 @@ TEST_COMPOSITE_END
 
 int main(int argc, char* argv[])
 {
-   if (argc > 2) {
-      UTIL_THROW("Too many arguments");
+   try {
+      if (argc > 2) {
+         UTIL_THROW("Too many arguments");
+      }
+   
+      McMdNsTestComposite runner;
+      if (argc == 2) {
+         runner.addFilePrefix(argv[1]);
+       }
+   
+      // Run all unit test methods
+      int failures = runner.run();
+
+      return (failures != 0);
+
+   } catch (...) {
+
+      std::cerr << "Uncaught exception in mcMd/tests/Test.cc" << std::endl;
+      return 1;
+
    }
-
-   McMdNsTestComposite runner;
-   if (argc == 2) {
-      runner.addFilePrefix(argv[1]);
-    }
-   runner.run();
-
 }
