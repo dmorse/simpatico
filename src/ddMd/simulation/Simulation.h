8786b03f0704a2c178511f8bfd764ad087d253a3
David Morse <morse012@umn.edu>
2018-02-18 22:53:57 -0600

    Removing vestigial DdMd::Simulation::[computeE][e]xternalEnergy() functions

--- src/ddMd/simulation/Simulation.h
+++ src/ddMd/simulation/Simulation.h
@@ -416,24 +416,6 @@ namespace DdMd
       */
       DMatrix<double> pairEnergies() const;
 
-      #ifdef SIMP_EXTERNAL
-      /**
-      * Compute pair energies for each pair of atom types.
-      *
-      * Reduce operation: Must be called on all nodes.
-      */
-      void computeExternalEnergy();
-
-      /**
-      * Return precomputed pair energies.
-      *
-      * Call only on master processor, after computePairEnergies.
-      *
-      * \return total pair energies (only correct on master node).
-      */
-      double externalEnergy() const;
-      #endif
-
       /**
       * Calculate and store kinetic stress.
       *
