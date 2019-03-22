5afc610eb3766440d6eb5b6b7ee3dc1e11461fa5
David Morse <morse012@umn.edu>
2019-03-21 23:58:40 -0500

    Removing MultiHarmonicDihedral from factories and tests

--- src/simp/tests/interaction/dihedral/DihedralTestComposite.h
+++ src/simp/tests/interaction/dihedral/DihedralTestComposite.h
@@ -6,13 +6,11 @@
 #include "TorsionForceTest.h"
 #include "CosineDihedralTest.h"
 #include "FourierDihedralTest.h"
-#include "MultiHarmonicDihedralTest.h"
 
 TEST_COMPOSITE_BEGIN(DihedralTestComposite)
 TEST_COMPOSITE_ADD_UNIT(TorsionForceTest);
 TEST_COMPOSITE_ADD_UNIT(CosineDihedralTest);
 TEST_COMPOSITE_ADD_UNIT(FourierDihedralTest);
-TEST_COMPOSITE_ADD_UNIT(MultiHarmonicDihedralTest);
 TEST_COMPOSITE_END
 
 #endif
