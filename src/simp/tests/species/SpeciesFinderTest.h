ff816a740cfd0b0b388bd2b4c02cbfbbfc729bd3
David Morse <morse012@umn.edu>
2020-06-19 15:07:15 -0500

    Adding SpeciesFinderTest, which passes.

---
+++ src/simp/tests/species/SpeciesFinderTest.h
@@ -0,0 +1,105 @@
+#ifndef SIMP_SPECIES_FINDER_TEST_H
+#define SIMP_SPECIES_FINDER_TEST_H
+
+#include <test/UnitTest.h>
+#include <test/UnitTestRunner.h>
+
+#include <simp/species/SpeciesFinder.h>
+#include <util/containers/DArray.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace Util;
+using namespace McMd;
+
+class SpeciesFinderTest : public UnitTest 
+{
+
+private:
+
+   SpeciesFinder finder;
+   int           nSpecies;
+   DArray<int>   nMolecules;
+   DArray<int>   nParts;
+
+public:
+
+   void setUp() 
+   { 
+      setVerbose(1);
+      nSpecies = 5;
+      nMolecules.allocate(nSpecies);
+      nParts.allocate(nSpecies);
+      nMolecules[0] = 12;
+      nParts[0]     = 8;
+      nMolecules[1] = 0;
+      nParts[1]     = 7;
+      nMolecules[2] = 9;
+      nParts[2]     = 3;
+      nMolecules[3] = 0;
+      nParts[3]     = 6;
+      nMolecules[4] = 5;
+      nParts[4]     = 8;
+
+      finder.allocate(nSpecies);
+      for (int i = 0; i < nSpecies; ++i) {
+         finder.setSpecies(i, nMolecules[i], nParts[i]);
+      }
+      finder.initialize();
+   } 
+
+   void tearDown() 
+   {}
+  
+   void testInitialize()
+   {
+      printMethod(TEST_FUNC);
+   }
+
+   void testFindMolecule()
+   {
+      printMethod(TEST_FUNC);
+
+      SpeciesFinder::Molecule context;
+      int is, im, j;
+      j = 0;
+      for (is = 0; is < nSpecies; ++is) {
+         for (im = 0; im < nMolecules[is]; ++im) {
+            finder.findMolecule(j, context);
+            TEST_ASSERT(context.speciesId == is);
+            TEST_ASSERT(context.moleculeId == im);
+            ++j;
+         }
+      }
+   }
+
+   void testFindPart()
+   {
+      printMethod(TEST_FUNC);
+
+      SpeciesFinder::Part context;
+      int is, im, ip, j;
+      j = 0;
+      for (is = 0; is < nSpecies; ++is) {
+         for (im = 0; im < nMolecules[is]; ++im) {
+            for (ip = 0; ip < nParts[is]; ++ip) {
+               finder.findPart(j, context);
+               TEST_ASSERT(context.speciesId == is);
+               TEST_ASSERT(context.moleculeId == im);
+               TEST_ASSERT(context.partId == ip);
+               ++j;
+            }
+         }
+      }
+   }
+
+};
+
+TEST_BEGIN(SpeciesFinderTest)
+TEST_ADD(SpeciesFinderTest, testInitialize)
+TEST_ADD(SpeciesFinderTest, testFindMolecule)
+TEST_ADD(SpeciesFinderTest, testFindPart)
+TEST_END(SpeciesFinderTest)
+
+#endif
--- src/simp/tests/species/SpeciesTestComposite.h
+++ src/simp/tests/species/SpeciesTestComposite.h
@@ -5,6 +5,7 @@
 
 #include "SpeciesGroupTest.h"
 #include "SpeciesTest.h"
+#include "SpeciesFinderTest.h"
 #include "HomopolymerTest.h"
 #include "MultiblockTest.h"
 #include "HomoRingTest.h"
@@ -12,6 +13,7 @@
 TEST_COMPOSITE_BEGIN(SpeciesTestComposite)
 TEST_COMPOSITE_ADD_UNIT(SpeciesGroupTest);
 TEST_COMPOSITE_ADD_UNIT(SpeciesTest);
+TEST_COMPOSITE_ADD_UNIT(SpeciesFinderTest);
 TEST_COMPOSITE_ADD_UNIT(HomopolymerTest);
 TEST_COMPOSITE_ADD_UNIT(MultiblockTest);
 TEST_COMPOSITE_ADD_UNIT(HomoRingTest);
